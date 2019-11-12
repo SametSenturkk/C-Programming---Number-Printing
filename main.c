e403298b5df8de40e02cdc574a51892f88b36764
Samet Şentürk <senturksamet123@gmail.com>
2019-11-12 17:28:27 +0300

    C Programming - Number Printig

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int number1,number2;
+	
+	printf("Enter a Number1 : ");
+	scanf("%d",& number1);
+	printf("Enter a Number2 : ");
+	scanf("%d", & number2);
+	printf("Total : %d",number1+number2);
+	
+	
+}
